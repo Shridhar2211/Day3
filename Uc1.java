e7d2b429510c9c198d65f45adfd1b3e7cd6a0bb7
Shridhar  Kolhatkar <88042097+Shridhar2211@users.noreply.github.com>
2021-09-28 10:14:31 +0530

    Add initial commit

---
+++ Uc1.java
@@ -0,0 +1,23 @@
+package com.Bridgeblabz;
+
+public class Employeewage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Empwageprogram");
+		 int IS_PRESENT = 1;  
+		 double Empcheck = Math.floor(Math.random() * 10) % 2;
+		 
+		 
+		   if (Empcheck == IS_PRESENT)
+			   
+		    System.out.println("Employee is Present");
+		   
+		   
+		    else
+		     System.out.println("Employee is Absent");
+
+	}
+
+}
+
